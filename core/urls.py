a8f3001fd031950827d59d266f2bd64fdf03ce24
BorisKolganov <bkolganov@me.com>
2016-09-23 19:24:08 +0300

    css fix

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from core.views import Index
+from core.views import Index, About
 
 urlpatterns = [
     url(r'^$', Index.as_view(), name='index'),
+    url(r'^about/$', About.as_view(), name='about')
 ]
--- core/views.py
+++ core/views.py
@@ -6,3 +6,7 @@ from django.views.generic import TemplateView
 
 class Index(TemplateView):
     template_name = 'index.html'
+
+
+class About(TemplateView):
+    template_name = 'about.html'
